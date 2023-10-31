dd6a955956f9798a6823577f84f7c83771af8bcd
David Espejo <david.espejo.repiso@gmail.com>
2023-10-31 18:44:55 +0100

    ultimos cambios

--- JS/mod-4-exercises/script.js
+++ JS/mod-4-exercises/script.js
@@ -1,36 +1,36 @@
 console.clear();
 
 const movie = {
-  title: "The Dark Knight",
-  director: "Christopher Nolan",
-  actors: "Christian Bale, Heath Ledger, Aaron Eckhart",
-  year: 2008,
-  category: "Action",
-  poster: "http://image.tmdb.org/t/p/w500//qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-  rating: 9,
-  description:
-    "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
+    title: "The Dark Knight",
+    director: "Christopher Nolan",
+    actors: "Christian Bale, Heath Ledger, Aaron Eckhart",
+    year: 2008,
+    category: "Action",
+    poster: "http://image.tmdb.org/t/p/w500//qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    rating: 9,
+    description:
+        "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
 };
 
 function createPosterElement(poster) {
-  const element = document.createElement("img");
-  element.src = poster;
-  element.className = "movie-poster";
-  return element;
+    const element = document.createElement("img");
+    element.src = poster;
+    element.className = "movie-poster";
+    return element;
 }
 
 function createTitleElement(title) {
-  const element = document.createElement("div");
-  element.className = "movie-title";
-  element.textContent = title;
-  return element;
+    const element = document.createElement("div");
+    element.className = "movie-title";
+    element.textContent = title;
+    return element;
 }
 
 function createDataElement(rating, year) {
-  const element = document.createElement("div");
-  element.className = "movie-data";
-  element.textContent = `Rating: ${rating} | ${year}`;
-  return element;
+    const element = document.createElement("div");
+    element.className = "movie-data";
+    element.textContent = `Rating: ${rating} | ${year}`;
+    return element;
 }
 
 function createButton() {
@@ -53,20 +53,37 @@ function hideData() {
 
 }
 
+function createDescriptionElement(description) {
+    const element = document.createElement("p");
+    element.className = "movie-description";
+    element.textContent = description;
+    return element;
+}
+
+function createDirectorElement(director) {
+    const element = document.createElement("p");
+    element.className = "movie-director";
+    element.textContent = director;
+    return element;
+}
+
 
 function createMovieElement(movieObj) {
-  const movieElement = document.createElement("div");
-  movieElement.className = "movie";
-  movieElement.appendChild(createPosterElement(movieObj.poster));
-  movieElement.appendChild(createButton());
-  
-  const movieDataContainer = document.createElement("div");
-  movieDataContainer.id = 'movie-data-container';
-  movieDataContainer.appendChild(createTitleElement(movieObj.title));
-  movieDataContainer.appendChild(createDataElement(movieObj.rating, movieObj.year));
-  
-  movieElement.appendChild(movieDataContainer);
-  return movieElement;
+    const movieElement = document.createElement("div");
+    movieElement.className = "movie";
+    movieElement.appendChild(createPosterElement(movieObj.poster));
+    movieElement.appendChild(createButton());
+
+    const movieDataContainer = document.createElement("div");
+    movieDataContainer.id = 'movie-data-container';
+    movieDataContainer.appendChild(createTitleElement(movieObj.title));
+    movieDataContainer.appendChild(createDataElement(movieObj.rating, movieObj.year));
+    movieDataContainer.appendChild(createDirectorElement(movieObj.director));
+    movieDataContainer.appendChild(createDescriptionElement(movieObj.description));
+    
+
+    movieElement.appendChild(movieDataContainer);
+    return movieElement;
 }
 
 const movieContainer = document.createElement("div");
